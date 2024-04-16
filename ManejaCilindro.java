d798ddfe88ee2716ec495d3bfc2abd9a91a31229
sumo0o <145007320+sumo0o@users.noreply.github.com>
2024-04-16 09:45:09 +0200

    Create ManejaCilindro.java

---
+++ ManejaCilindro.java
@@ -0,0 +1,14 @@
+package primercodigo;
+
+public class ManejaCilindro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cilindro tubo;
+		tubo=new Cilindro(2.0,5.3,"azul");
+		
+		
+
+	}
+
+}
